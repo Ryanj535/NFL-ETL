7d65020326dece360645bd7ae5d2484c1dcdc61e
KbHarold <kyron_harold@yahoo.com>
2019-04-11 08:37:44 -0400

    Got the new table to reflect in sqlite and is callable

---
+++ app.py
@@ -0,0 +1,54 @@
+##################################################
+#Import Dependencies
+##################################################
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+import datetime as dt
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///nfl_etf.sqllite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the table
+Stats = Base.classes.qb_stats2
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/jsonified<br/>"
+    )
+
+
+@app.route("/api/v1.0/jsonified")
+def stations():
+    """Return a list of all stations"""
+    session = Session(engine)
+    return jsonify(Stats)
